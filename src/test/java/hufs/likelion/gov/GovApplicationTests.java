79e8c4f96eb7ff8c1068490e2a9fe34a27de4a9c
SeolHuiGwan9478 <shg5977@naver.com>
2024-08-18 19:08:14 +0900

    first commit

---
+++ src/test/java/hufs/likelion/gov/GovApplicationTests.java
@@ -0,0 +1,13 @@
+package hufs.likelion.gov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
