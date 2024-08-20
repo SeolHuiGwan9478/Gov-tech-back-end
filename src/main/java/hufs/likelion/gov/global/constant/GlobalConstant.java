76b35d331112aaaf99e06e3ed9541d18b2ed5779
SeolHuiGwan9478 <shg5977@naver.com>
2024-08-20 16:30:47 +0900

    Feat: Member 엔티티 CarePost에 적용 완료

--- src/main/java/hufs/likelion/gov/global/constant/GlobalConstant.java
+++ src/main/java/hufs/likelion/gov/global/constant/GlobalConstant.java
@@ -2,4 +2,5 @@ package hufs.likelion.gov.global.constant;
 
 public class GlobalConstant {
     public static String NOT_FOUND_CARE_POST_ERR_MSG = "Not Found: CarePost";
+    public static String NOT_FOUND_MEMBER_ERR_MSG = "Not Found: Member";
 }
