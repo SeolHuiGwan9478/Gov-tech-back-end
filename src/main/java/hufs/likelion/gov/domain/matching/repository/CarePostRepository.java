8ab710f0d5b5e28c8521b05f90c96d89d4186059
SeolHuiGwan9478 <shg5977@naver.com>
2024-08-20 14:33:25 +0900

    Feat: 특정 CarePost 조회 기능 구현 완료

--- src/main/java/hufs/likelion/gov/domain/matching/repository/CarePostRepository.java
+++ src/main/java/hufs/likelion/gov/domain/matching/repository/CarePostRepository.java
@@ -3,5 +3,7 @@ package hufs.likelion.gov.domain.matching.repository;
 import hufs.likelion.gov.domain.matching.entity.CarePost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CarePostRepository extends JpaRepository<CarePost, Long> {
 }
--- src/main/java/hufs/likelion/gov/domain/matching/service/CarePostService.java
+++ src/main/java/hufs/likelion/gov/domain/matching/service/CarePostService.java
@@ -1,9 +1,12 @@
 package hufs.likelion.gov.domain.matching.service;
 
+import hufs.likelion.gov.domain.matching.dto.CarePostResponse;
 import hufs.likelion.gov.domain.matching.dto.CarePostsData;
 import hufs.likelion.gov.domain.matching.dto.CarePostsResponse;
 import hufs.likelion.gov.domain.matching.entity.CarePost;
 import hufs.likelion.gov.domain.matching.repository.CarePostRepository;
+import hufs.likelion.gov.global.constant.GlobalConstant;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static hufs.likelion.gov.global.constant.GlobalConstant.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,6 +32,22 @@ public class CarePostService {
                 .totalPages(carePostsPage.getTotalPages())
                 .curPage(carePostsPage.getNumber())
                 .data(carePostsData)
+                .build();
+    }
+
+    public CarePostResponse findCarePost(Long carePostId){
+        // write authentication
+        CarePost findCarePost = carePostRepository.findById(carePostId).orElseThrow(
+                () -> new EntityNotFoundException(NOT_FOUND_CARE_POST_ERR_MSG)
+        );
+        return CarePostResponse.builder()
+                .id(findCarePost.getId())
+                .title(findCarePost.getTitle())
+                .content(findCarePost.getContent())
+                .price(findCarePost.getPrice())
+                .address(findCarePost.getAddress())
+                .createdAt(findCarePost.getCreatedAt())
+                .updatedAt(findCarePost.getUpdatedAt())
                 .build();
     }
 }
