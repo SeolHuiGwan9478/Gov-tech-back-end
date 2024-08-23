d5932a8f378e4e3e18952c03f00d3fb593e8a022
SeolHuiGwan9478 <shg5977@naver.com>
2024-08-23 16:59:14 +0900

    Feat: ChatRoom 생성 기능 구현 완료

--- src/main/java/hufs/likelion/gov/domain/chatting/dto/GetChatRoomsData.java
+++ src/main/java/hufs/likelion/gov/domain/chatting/dto/GetChatRoomsData.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class FindChatRoomsData {
+public class GetChatRoomsData {
     private Long id;
     private String roomId;
     private String title;
---
+++ src/main/java/hufs/likelion/gov/domain/chatting/entity/Message.java
@@ -0,0 +1,31 @@
+package hufs.likelion.gov.domain.chatting.entity;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "messages")
+public class Message {
+    @Id
+    @Field(value = "_id", targetType = FieldType.OBJECT_ID)
+    private String id;
+    private String roomId;
+    private Long senderId;
+    private String senderName;
+    private String message;
+    private MessageStatus status;
+    @CreatedDate
+    private LocalDateTime date;
+}
