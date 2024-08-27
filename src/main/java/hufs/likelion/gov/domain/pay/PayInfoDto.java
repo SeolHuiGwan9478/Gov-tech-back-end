a072472dba8194f142a09f0afb05d77b74f7e04f
Wuxxeong <sws326666@gmail.com>
2024-08-27 15:12:35 +0900

    Feature : 카카오 페이 결제 관련 DTO 생성

---
+++ src/main/java/hufs/likelion/gov/domain/pay/PayInfoDto.java
@@ -0,0 +1,9 @@
+package hufs.likelion.gov.domain.pay;
+
+import lombok.Getter;
+
+@Getter
+public class PayInfoDto {
+    private int price;
+    private String itemName;
+}
---
+++ src/main/java/hufs/likelion/gov/domain/pay/dto/PayApproveResDto.java
@@ -0,0 +1,10 @@
+package hufs.likelion.gov.domain.pay.dto;
+
+import hufs.likelion.gov.domain.pay.response.Amount;
+
+public class PayApproveResDto {
+    private Amount amount;
+    private String item_name;
+    private String created_at;
+    private String approved_at;
+}
