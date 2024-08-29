950fe077295c6b75c523acac6019a5af091a4a14
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-08-29 15:32:10 +0900

    Feat : Validator 코드 추가 Validator 코드 추가

--- src/main/java/hufs/likelion/gov/global/constant/GlobalConstant.java
+++ src/main/java/hufs/likelion/gov/global/constant/GlobalConstant.java
@@ -7,4 +7,15 @@ public class GlobalConstant {
     public static String NOT_FOUND_CHAT_ROOM_MEMBER_ERR_MSG = "Not Found: ChatRoomMember";
     public static String NOT_FOUND_AUTH_HEADER_ERR_MSG = "Not Found: Authorization Header";
     public static String NOT_FOUND_COMPLAIN_ERR_MSG = "Not Found: Complain";
+    public static String NOT_FOUND_SUPER_ADMIN_ERR_MSG = "Not Found: SuperAdmin";
+    public static String ALREADY_EXIST_MEMBER_ID_ERR_MSG = "Already Exist: MemberId";
+    public static String REGISTER_SUPER_ADMIN_ERR_MSG = "Can't Register : SuperAdmin";
+    public static String REGISTER_MANAGER_ERR_MSG = "Can't Register : SuperAdmin";
+    public static String REGISTER_USER_ERR_MSG = "Can't Register : Member";
+    public static String REGISTER_AUTH_ERR_MSG = "Authentication Error : Only SuperAdmin can register Manager";
+    public static String INVALID_REFRESH_TOKEN_ERR_MSG = "Invalid token : RefreshToken";
+    public static String AUTHENTICATION_SUPER_ADMIN_ERR_MSG = "Authentication Error : Only SuperAdmin can access";
+    public static String AUTHENTICATION_MANAGER_ERR_MSG = "Authentication Error : Only Manager can access";
+    public static String AUTHENTICATION_PROTECTOR_ERR_MSG = "Authentication Error : Only Protector can access";
+    public static String AUTHENTICATION_CARETAKER_ERR_MSG = "Authentication Error : Only CareTaker can access";
 }
